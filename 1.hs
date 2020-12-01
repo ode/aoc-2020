da6c68de677af5d09ceb605141b175390e286b11
LordPos <34184577+LordPos@users.noreply.github.com>
2020-12-01 11:08:42 +0530

    Create 1.hs
    Haskell!

---
+++ 1.hs
@@ -0,0 +1,6 @@
+num a = product $ head $ filter ((== 2020).sum) (sequence [a,a]) -- this is sequence [a,a,a] for part 1
+
+prepare :: String -> [Int]
+prepare = map read . lines
+
+main = readFile "1.txt" >>= print . num . prepare
